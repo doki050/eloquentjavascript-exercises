57d55b161aafc4cf719b33460e64277d91650e43
Kovács Benjamin Márk <kovacs.benjamin.mark99@gmail.com>
2021-06-08 17:47:11 +0200

    class declaration

--- js-factorial.js
+++ js-factorial.js
@@ -181,3 +181,31 @@ console.log("\n")
 let arrays = [[1, 2, 3], [4, 5], [6]];
 console.log(arrays.reduce((flat, current) => flat.concat(current), []))
 
+console.log("\n");
+//11.
+// Class
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y);
+  }
+
+  minus(other) {
+    return new Vec(this.x - other.x, this.y - other.y);
+  }
+
+  get length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
